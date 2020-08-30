bff80a54659e5e827cf67ad6d5c3fb55ca0630cf
Ricardo Capelli <ricki.capelli@gmail.com>
2020-08-30 19:13:59 -0300

    ejercicio terminado

--- script.js
+++ script.js
@@ -6,32 +6,49 @@ const classNames = {
 }
 
 const list = document.getElementById('todo-list')
-let itemCountSpan = document.getElementById('item-count').innerText
-let uncheckedCountSpan = document.getElementById('unchecked-count').innerText
+const itemCountSpan = document.getElementById('item-count')
+let uncheckedCountSpan = document.getElementById('unchecked-count')
 
 const table = document.createElement('table')
-const tbody = document.createElement('tbody')
 const thead1 = document.createElement('th')
 const thead2 = document.createElement('th')
 const thead3 = document.createElement('th')
+const thead4 = document.createElement('th')
 const tr = document.createElement('tr')
 
 function addTodo() {
-  checkStatus()
-  var tarea = prompt(message())
-  if(tarea != null)
-  var newH = confirm("Quieres agregar: " + tarea + "?")
-  if(newH){
-    newElement(tarea)
+  let tarea = prompt(message())
+  if(tarea === "" || tarea === null){
+    alert(messageError())
+  }else{
+    var newH = confirm("Quieres agregar: " + tarea + "?")
+    if(newH){
+      checkStatus()
+      newElement(tarea)
+      addItemCount()
+    }
   }
-  addItemCount()
-  searchUnchecked()
 }
 
+function createTable(){
+  
+  table.classList.add('table', 'text-center')
+  list.append(table)
+  table.append(tr)
+  tr.append(thead1,thead2,thead3,thead4)
+  thead1.innerText = "Tareas"
+  thead2.innerText = "Descripción"
+  thead3.innerText = "¿La hiciste?"
+  //table.append(tbody)
+}
 function addItemCount(){
-  parseInt(itemCountSpan)
-  itemCountSpan++
-  document.getElementById('item-count').innerText = itemCountSpan
+  let count = parseInt(itemCountSpan.innerText)
+  count++
+  itemCountSpan.innerText = count.toString()
+
+  let unchecked = parseInt(uncheckedCountSpan.innerText)
+  unchecked++
+  uncheckedCountSpan.innerText = unchecked.toString()
 }
 
 function searchUnchecked(){
@@ -41,20 +58,15 @@ function searchUnchecked(){
   document.getElementById('unchecked-count').innerText = uncheckedCountSpan
 }
 
-
-
-function createTable(){
-  
-  table.classList.add('table')
-  list.append(table)
-  table.append(tr)
-  tr.append(thead1,thead2,thead3)
-  thead1.innerText = "Tareas"
-  thead2.innerText = "Descripción"
-  thead3.innerText = "¿La hiciste?"
-  table.append(tbody)
+function refreshState(checkbox){
+  let count = parseInt(uncheckedCountSpan.innerText)
+    if(checkbox.checked == true){
+      count--
+    }else{
+      count++
+    }
+  uncheckedCountSpan.innerText = count.toString()
 }
-
 
 function checkStatus(){
   if(list.firstChild == null){
@@ -62,12 +74,13 @@ function checkStatus(){
   }
 }
 
-
-
 function message(){
   return "Ingresá una nueva tarea a realizar"
 }
 
+function messageError(){
+  return "No ingresaste nada!"
+}
 
 function newElement(tarea){
   let tr = document.createElement('tr')
@@ -81,12 +94,12 @@ function newElement(tarea){
             td.innerText = tarea
             break;
         case 1: 
-            td.innerHTML = "<input type='text' placeholder='descripcion'>"
+            td.innerHTML = "<textarea rows='5' cols='45' placeholder='descripcion de la tarea'>"
             break;    
         case 2:
-            td.innerHTML = "<input type='checkbox' name='checkbox'>"
+            td.innerHTML = "<input type='checkbox' name='checkbox' onClick='refreshState(this)'>"
+            break;
       }
-  
     }
 }
 
