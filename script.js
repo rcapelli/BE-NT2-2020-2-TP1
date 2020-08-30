57290eb2ff841a357793ad54e6c9293929c34425
Ricardo Capelli <ricki.capelli@gmail.com>
2020-08-29 21:36:57 -0300

    avance ejercicio(falta manejo checkbox)

--- script.js
+++ script.js
@@ -6,44 +6,87 @@ const classNames = {
 }
 
 const list = document.getElementById('todo-list')
-const itemCountSpan = document.getElementById('item-count')
-const uncheckedCountSpan = document.getElementById('unchecked-count')
+let itemCountSpan = document.getElementById('item-count').innerText
+let uncheckedCountSpan = document.getElementById('unchecked-count').innerText
+
+const table = document.createElement('table')
+const tbody = document.createElement('tbody')
+const thead1 = document.createElement('th')
+const thead2 = document.createElement('th')
+const thead3 = document.createElement('th')
+const tr = document.createElement('tr')
 
 function addTodo() {
   checkStatus()
-
   var tarea = prompt(message())
   if(tarea != null)
   var newH = confirm("Quieres agregar: " + tarea + "?")
   if(newH){
     newElement(tarea)
   }
+  addItemCount()
+  searchUnchecked()
 }
+
+function addItemCount(){
+  parseInt(itemCountSpan)
+  itemCountSpan++
+  document.getElementById('item-count').innerText = itemCountSpan
+}
+
+function searchUnchecked(){
+  let qCheckbox = document.getElementsByName('checkbox')
+  parseInt(uncheckedCountSpan)
+  uncheckedCountSpan++
+  document.getElementById('unchecked-count').innerText = uncheckedCountSpan
+}
+
+
+
+function createTable(){
+  
+  table.classList.add('table')
+  list.append(table)
+  table.append(tr)
+  tr.append(thead1,thead2,thead3)
+  thead1.innerText = "Tareas"
+  thead2.innerText = "Descripción"
+  thead3.innerText = "¿La hiciste?"
+  table.append(tbody)
+}
+
 
 function checkStatus(){
   if(list.firstChild == null){
-    var table = document.createElement("table")
-    var thead1,thead2,thead3 = document.createElement("th")
-    thead1.innerText = "asd1"
-    thead2.innerText = "qwe2"
-    thead3.innerText = "qweq3"
-    table.appendChild(thead1)
-    table.appendChild(thead2)
-    table.appendChild(thead3)
-
+    createTable()
   }
 }
+
+
 
 function message(){
   return "Ingresá una nueva tarea a realizar"
 }
 
+
 function newElement(tarea){
-  var li = document.createElement("li")
-  //list.innerHTML = "<li>"+tarea+"</li>"
-  list.appendChild(li)
-  li.innerText = tarea
+  let tr = document.createElement('tr')
+  table.appendChild(tr)
+  let qTh = document.getElementsByTagName('th').length
+    for(let x=0;x<qTh;x++){
+    let td = document.createElement("td")
+    tr.appendChild(td)
+      switch(x){
+        case 0:
+            td.innerText = tarea
+            break;
+        case 1: 
+            td.innerHTML = "<input type='text' placeholder='descripcion'>"
+            break;    
+        case 2:
+            td.innerHTML = "<input type='checkbox' name='checkbox'>"
+      }
+  
+    }
 }
-
-
 
